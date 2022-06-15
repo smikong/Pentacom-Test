016f88e1f3731338c6fd47f0f6615823ab026db4
smikong <leimeter.roland@nwmgroup.hu>
2022-06-15 19:13:52 +0200

    [PENTACOM - 2] - Create database schema with model and base controller

---
+++ app/Models/Todo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
